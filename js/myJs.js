631c9bba934c0352c6526b24426dd20433b6580f
tuandatN <25832997+tuandatN@users.noreply.github.com>
2021-07-03 16:52:02 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -73,7 +73,7 @@ $('#no').click(() => {
 // generate text in input
 function textGenerate() {
     var n = "";
-    var text = " Tại vì anh hiền 😙😙😙 :<<<<<<< ";
+    var text = " Tại vì anh đẹp trai quá 😙😙😙 :<<<<<<< ";
     var a = Array.from(text);
     var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
     var count = textVal.length;
